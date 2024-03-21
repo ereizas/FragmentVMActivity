4eebdbfdaf7422bc2c45186ba416452df01d1449
Eric Reizas <tuo17224@temple.edu>
2024-03-21 11:32:36 -0400

    Set up DiceViewModel to take in an Int and return a LiveData object with an int in it

--- app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
+++ app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
@@ -1,7 +1,19 @@
 package edu.temple.diceroll
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DiceViewModel : ViewModel() {
-    
+    private val numRolled = MutableLiveData<Int>()
+
+    fun setNumRolled(_numRolled:Int)
+    {
+        numRolled.value=_numRolled
+    }
+
+    fun getNumRolled(): LiveData<Int>
+    {
+        return numRolled
+    }
 }
